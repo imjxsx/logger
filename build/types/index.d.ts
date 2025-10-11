cd3c51b3b251a637b91e3a4891107355f1fc3473
I'm Jxsx <jxsxdxnxxl@gmail.com>
2025-10-11 10:57:57 -0300

    Delete build directory

--- build/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export type Nullable<T> = T | undefined | null;
-export type LoggerLevel = "OFF" | "FATAL" | "ERROR" | "WARN" | "INFO" | "DEBUG" | "TRACE";
-export interface LoggerOptions {
-    name?: Nullable<string>;
-    timestamp?: Nullable<() => string>;
-    level?: Nullable<LoggerLevel>;
-    colorize?: Nullable<boolean>;
-    json?: Nullable<boolean>;
-}
--- build/utils/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isRecord(value: any): value is Record<string, any>;
-export declare function stringify(parts: any[], separator?: string): string;
